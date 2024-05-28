074900217bcc2822164e0eceada96151d2b52f67
IT22079572 <it22079572@my.sliit.lk>
2024-05-29 01:14:23 +0530

    initial commit - Total Webpage

---
+++ main.js
@@ -0,0 +1,82 @@
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+// header container
+ScrollReveal().reveal(".header__container h1", scrollRevealOption);
+
+ScrollReveal().reveal(".header__container h4", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".header__container .btn", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+// about container
+ScrollReveal().reveal(".about__container .section__header", scrollRevealOption);
+ScrollReveal().reveal(".about__container .section__subheader", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".about__container .about__flex", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".about__container .btn", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+// discover container
+ScrollReveal().reveal(".discover__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".discover__card__content", {
+  ...scrollRevealOption,
+  interval: 500,
+  delay: 200,
+});
+
+// blogs container
+ScrollReveal().reveal(".blogs__card", {
+  duration: 1000,
+  interval: 400,
+});
+
+// journals container
+ScrollReveal().reveal(".journals__card", {
+  ...scrollRevealOption,
+  interval: 400,
+});
+
+
+// Show scroll-to-top button when user scrolls down
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
+// Scroll to top function
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// Attach click event listener to scroll-to-top button
+document.getElementById("scrollToTopBtn").addEventListener("click", scrollToTop);
+
